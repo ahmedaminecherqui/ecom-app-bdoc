715310a799212b14b0d1724f6ae041a4bdbe562a
ahmedaminecherqui <127991970+ahmedaminecherqui@users.noreply.github.com>
2025-12-26 21:15:30 +0100

    Added analytics-service with Kafka Streams for real-time event aggregation.

---
+++ analytics-service/src/main/java/ma/emsi/cherqui/analyticsservice/service/BillAnalyticsService.java
@@ -0,0 +1,25 @@
+package ma.emsi.cherqui.analyticsservice.service;
+
+import ma.emsi.cherqui.analyticsservice.model.BillEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class BillAnalyticsService {
+
+    @Bean
+    public Function<KStream<String, BillEvent>, KStream<String, Long>> billCounter() {
+        return input -> input
+                .map((k, v) -> new KeyValue<>(String.valueOf(v.getCustomerId()), v))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.JavaSerialization(BillEvent.class)))
+                .count(Materialized.as("bill-counts"))
+                .toStream();
+    }
+}
